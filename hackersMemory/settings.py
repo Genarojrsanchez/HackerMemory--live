8b39a8e6bd55b51c615854611ddfed9a9a57cf1c
Genaro Sanchez <genarorwc@yahoo.com>
2020-04-29 19:05:38 -0700

    initial commit

--- hackersMemory/settings.py
+++ hackersMemory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')z+7q%qsv@5(5!wdkkc!f$fo&s(*10axe*9)p6b5_(mj5*t7y5o9+'
+SECRET_KEY = ')z+7q%qv@5(5!wkkc!f$fo&s(*10axe*9)p6b5_(mj5*t7y5o9+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = FALSE
@@ -120,10 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
